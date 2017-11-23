c3a05db4a08b0d5e6130c17f7f94537bbce3b1dc
ShinKhmer <cmkhmer@gmail.com>
2017-11-23 16:17:27 +0100

    Récupération de fichier
    - Fichier récupéré
    - Lecture des lignes
    - Toutes les lignes ont été placé dans une chaîne de caractères (tableau)

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define SIZE 100
 
 int main(int argc, char **argv)
 {
 
     FILE* explore = NULL;
+    char *line = NULL;
     char *text = NULL;
 
     int i = 0;
+    int cnt = 0;
 
 
+    line = malloc( sizeof(char) * SIZE );
+    line[0] = '\0';                         // init line
     text = malloc( sizeof(char) * SIZE );
+    text[0] = '\0';
 
 
     // File recovery
@@ -19,12 +25,21 @@ int main(int argc, char **argv)
 
     if( explore != NULL ){
 
-
         // INSTRUCTIONS
-        while( fgets( text, SIZE, explore) != NULL ){
-            printf( "%s\n", text );
-            printf( "%d", strlen(text) );
+        cnt = 0;    // Each line read will make cnt++
+        while( fgets( line, SIZE, explore) != NULL ){
+            printf( "%s\n", line );
+            if(cnt == 0){
+                strcpy( text, line );
+            }
+            else{
+                strcat( text, line );
+            }
+            printf( "%d\n", strlen(line) );
+            cnt++;
         }
+        printf( "Texte final: %s\n", text );
+        printf( "Taille texte: %d\n", strlen(text) );
 
 
         // ECRITURE
@@ -41,8 +56,8 @@ int main(int argc, char **argv)
 
 
 
-
-
+    free(text);
+    free(line);
 
     return 0;
 }
