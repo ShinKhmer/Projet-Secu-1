20c57c46c6b08b7c3ea879b77f9a82a46f4708a2
ShinKhmer <cmkhmer@gmail.com>
2017-11-24 00:58:23 +0100

    Fonction print_result
    Affiche
    - Caractère
    - 8 bits correspondants
    - 2 caractères hexa correspondants
    
    Début de la prise en compte du fichier de matrice.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 {
 
     FILE* explore = NULL;
+    FILE* matrice = NULL;
     char *line = NULL;
     char *text = NULL;
     int *bin = NULL;
@@ -25,11 +26,12 @@ int main(int argc, char **argv)
     bin = malloc( sizeof(int) * SIZE * 8 );
     init(bin);
 
-    hexa = malloc( sizeof(char) * SIZE / 4 );
+    hexa = malloc( sizeof(char) * SIZE * 8 / 4 );
     hexa[0] = '\0';
 
     // File pointer
     explore = fopen("texte.txt", "r+");     // Open file / r+: read and write
+    matrice = fopen("matrice.txt", "r");
 
     if( explore != NULL ){
 
@@ -38,15 +40,19 @@ int main(int argc, char **argv)
         size_text = strlen(text);
 
 
-        // CONVERT BINARY TO HEXA
+        // CONVERT CHAR TO BINARY
         convert_char_to_bin( text, bin, size_text );
         printf( "\nTaille texte: %d\n", strlen(text) );
 
-        print_bin( text, bin, size_text );
+
+        // CONVERT BINARY TO HEXA => NOT NECCESSARY
+        convert_bin_to_hexa( bin, hexa, size_text );
+
+        print_result( text, bin, hexa, size_text );
 
 
-
-
+        // SEARCH
+        read_matrice( matrice );
 
         // ECRITURE
         //fputs( 'A', text );
@@ -78,8 +84,6 @@ void init( int *tab ){
     }
 }
 
-
-
 void read_file( char *file, char *line, char* text ){
 
     int cnt = 0;                            // Each line read will make cnt++
@@ -103,7 +107,6 @@ void read_file( char *file, char *line, char* text ){
 
 }
 
-
 void convert_char_to_bin( char *text, int *bin, int size ){
     int i = 0;
     int j = 0;
@@ -126,44 +129,105 @@ void convert_char_to_bin( char *text, int *bin, int size ){
     }
 
     cnt = 0;
-
-    /*for( i = 0; i < size * 8; i++ ){
-
-        if( i == 0 ){
-            printf("%c: ", text[cnt]);
-            cnt++;
-        }
-
-        if( i != 0 && i % 8 == 0 ){
-            printf("\n%c: ", text[cnt]);
-            cnt++;
-        }
-
-        printf("%d" , bin[i]);
-    }*/
-
 }
 
-void print_bin( char *text, int* bin, int size ){
+void convert_bin_to_hexa( int *bin, char *hexa, int size ){
+    int i = 0;
+    int j = 0;
+    int cnt = 0;
+    int result = 0;
+
+    for( i = 0; i < size * 8 / 4; i++ ){
+
+        // SUM 4 BITS
+        result = 0;
+        for( j = 0; j < 4; j++ ){
+            if( bin[(j + cnt)] == 1 ){
+                switch( j ){
+                    case 0: result += 8;
+                            break;
+                    case 1: result += 4;
+                            break;
+                    case 2: result += 2;
+                            break;
+                    case 3: result += 1;
+                            break;
+                    default:
+                            printf("Il y a une couille dans le pâté");
+                            break;
+                }
+            }
+        }
+
+        // CONVERT TO HEXA
+        if( result >= 0 && result <= 9 ){
+            hexa[i] = result + 48;          // ASCII 0 to 10
+        }
+        else if( result >= 10 && result <= 15 ){
+            hexa[i] = result + 55;          // ASCII A to F
+        }
+        else{
+            printf("error");
+        }
+
+        cnt += 4;
+    }
+}
+
+void print_result( char *text, int* bin, char *hexa, int size ){
     int cnt = 0;
     int i = 0;
 
     for( i = 0; i < size * 8; i++ ){
 
-        if( i == 0 ){
+        if( i == 0 ){                                           // print first char
             printf("%c: ", text[cnt]);
+        }
+
+        if( i != 0 && i % 8 == 0 ){                             // print hexa, return to the line then print char
+            printf(" => %c %c\n%c: ", hexa[cnt*2], hexa[cnt*2+1], text[cnt+1]);
             cnt++;
         }
 
-        if( i != 0 && i % 8 == 0 ){
-            printf("\n%c: ", text[cnt]);
-            cnt++;
-        }
+        printf("%d", bin[i]);                                   // print binary
 
-        printf("%d" , bin[i]);
+        if( i == size * 8 - 1 )                                 // print last hexa
+            printf(" => %c %c ", hexa[cnt*2], hexa[cnt*2+1] );
+
+
+
     }
+
+    printf("\n\n");
 }
 
 
 
+
+
+
+
+
+
+
+
+
+/*void print_tab_char( char *tab, int size, int separate ){
+    int i = 0;
+
+    for( i = 0; i < size; i++ ){
+        if( i != 0 && i % separate == 0 )
+            printf("\n");
+
+        printf("%c", tab[i]);
+    }
+}
+
+void print_tab_int( int *tab, int size ){
+    int i = 0;
+
+    for( i = 0; i < size; i++ ){
+        printf("%d\n", tab[i]);
+    }
+}*/
 
