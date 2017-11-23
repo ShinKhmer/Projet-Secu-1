e33f5911628c1a8c40486fcb2129c772cec0a1ee
ShinKhmer <cmkhmer@gmail.com>
2017-11-23 17:03:35 +0100

    Fonction read_file() créée

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define SIZE 100
 
+
 int main(int argc, char **argv)
 {
 
@@ -10,36 +11,47 @@ int main(int argc, char **argv)
     char *line = NULL;
     char *text = NULL;
 
+
+
     int i = 0;
     int cnt = 0;
 
 
     line = malloc( sizeof(char) * SIZE );
     line[0] = '\0';                         // init line
+
     text = malloc( sizeof(char) * SIZE );
-    text[0] = '\0';
 
-
-    // File recovery
-    explore = fopen("texte.txt", "r+");    // Open file / r+: read and write
+    // File pointer
+    explore = fopen("texte.txt", "r+");     // Open file / r+: read and write
 
     if( explore != NULL ){
 
-        // INSTRUCTIONS
-        cnt = 0;    // Each line read will make cnt++
+        // FILE RECOVERY
+        read_file( explore, line, text, SIZE);
+        /*cnt = 0;                            // Each line read will make cnt++
         while( fgets( line, SIZE, explore) != NULL ){
             printf( "%s\n", line );
-            if(cnt == 0){
+            if(cnt == 0){                   // 1st line => copy
                 strcpy( text, line );
             }
-            else{
+            else{                           // next lines => concat
                 strcat( text, line );
             }
             printf( "%d\n", strlen(line) );
             cnt++;
-        }
+        }*/
         printf( "Texte final: %s\n", text );
         printf( "Taille texte: %d\n", strlen(text) );
+
+
+        // CONVERT BINARY TO HEXA
+        /*for( i = 0; i < strlen(text); i++ ){
+            tab[i]
+        }*/
+
+
+
 
 
         // ECRITURE
@@ -61,3 +73,30 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+
+void read_file( char *file, char *line, char* text, int size ){
+
+    int cnt = 0;                            // Each line read will make cnt++
+
+    if( text != NULL ){
+    text[0] = '\0';
+
+        while( fgets( line, SIZE, file) != NULL ){
+            printf( "%s\n", line );
+            if(cnt == 0){                   // 1st line => copy
+                strcpy( text, line );
+            }
+            else{                           // next lines => concat
+                strcat( text, line );
+            }
+            printf( "%d\n", strlen(line) );
+            cnt++;
+        }
+    }else{
+        printf("L'allocation de la variable text n'a pas fonctionné.");
+    }
+
+}
+
+
